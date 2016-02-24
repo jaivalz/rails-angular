a12a9c99e6420803846ef3e5aa291842b6f25bde
Jaime <jaivalz@yahoo.com>
2016-02-25 00:36:54 +0100

    Added actions for posts service and controller

--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -1,28 +1,25 @@
-var app = angular.module('jangular');
+var app = angular.module('flapperNews');
 
-app.controller('PostsController', [
+app.controller('PostsCtrl', [
   '$scope',
-  '$stateParams',
   'posts',
+  'post',
+  function($scope, posts, post) {
+    $scope.post = post;
 
-  function($scope, $stateParams, posts) {
-    $scope.post = posts.posts[$stateParams.id];
-
-    $scope.addComment = function() {
-      if ($scope.body === '') {
-        return;
-      } else {
-        $scope.post.comments.push({
-          body: $scope.body,
-          author: 'user',
-          upvotes: 0
-        });
-        $scope.body = '';
-      }
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      posts.addComment(post.id, {
+        body: $scope.body,
+        author: 'user',
+      }).success(function(comment) {
+        $scope.post.comments.push(comment);
+      });
+      $scope.body = '';
     };
 
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
+    $scope.incrementUpvotes = function(comment){
+      posts.upvoteComment(post, comment);
     };
-  }
-]);
+
+}]);
